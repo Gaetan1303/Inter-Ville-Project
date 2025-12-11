ac753f934e7bcc9724e2eec6cbb5ef0f773b98b6
MathCT <mathieu.chatrain@gmail.com>
2025-12-11 14:08:06 +0100

    commit rapide pour port Front

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,8 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3000, // Port du serveur de développement frontend
-    host: true, // Permet l'accès depuis le réseau local
-  },
 })
