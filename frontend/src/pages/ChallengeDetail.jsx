b31b3f6da4a6509db92560449393726c08f6f9a9
Achraf Jarhou <achraf.jarhou@laplateforme.io>
2025-12-11 11:31:45 +0100

    creation page qui affiche la liste des commentaire et amelioration de page detail d'un challenge

--- frontend/src/pages/ChallengeDetail.jsx
+++ frontend/src/pages/ChallengeDetail.jsx
@@ -1,9 +1,35 @@
 import React from "react";
 import { useParams } from "react-router-dom";
+import CommentList from "../components/CommentList";
 
-const ChallengeDetail = () => {
-  const { id } = useParams();
-  return <div>on est sur le challenge {id}</div>;
-};
 
-export default ChallengeDetail;
+export default function ChallengePage() {
+  const { id } = useParams();   
+
+  return (
+    <div className="container">
+      <article className="detail">
+        <h2>title</h2>
+        <div className="meta">
+          Code • difficile • Posté par{" "}
+          Achraf
+        </div>
+        <p>description</p>
+      
+
+        <div style={{ marginTop: "12px" }}>
+          <button>Participer</button>
+        </div>
+      </article>
+
+      <section style={{ marginTop: "20px" }}>
+        <h3>Commentaires</h3>
+        <CommentList  />
+        <CommentList  />
+        <CommentList  />
+        <CommentList  />
+
+      </section>
+    </div>
+  );
+}
